09029e9dc6a7d62b12c71faa348a872e83ba30ec
Riyaz <skriyaz016@yahoo.com>
2018-10-30 19:04:56 +0530

    first commit

---
+++ basicprograms/simpleinterest.py
@@ -0,0 +1,5 @@
+pri=eval(input("Enter the principle:"))
+rate=eval(input("Enter the rate:"))
+tenu=eval(input("Enter the tenure:"))
+si=(pri*rate*tenu)/100
+print("Simple intered:",si)
---
+++ ftocel.py
@@ -0,0 +1,3 @@
+r=eval(input("Enter the radiouos of area:"))
+area=3.14*r*r
+print("Area of a circle is:",area)
